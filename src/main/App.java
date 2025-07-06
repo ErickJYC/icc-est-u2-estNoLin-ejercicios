1dc306af2dba632fd69a9ebb2105d4c6ea2c23f9
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-07-06 14:31:35 -0500

    feat (001) : Se ha realizado el primer ejercicio no podemos aun probarlos porque falta hacer los demas

--- src/main/App.java
+++ src/main/App.java
@@ -1,6 +1,8 @@
 package main;
 
+import main.Ejercicio_01_insert.InsertBST;
 import utils.StudentValidator;
+import main.Materia.Models.Node;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,6 +11,16 @@ public class App {
         System.out.println("👤 Estudiante: " + estudiante.nombre());
         System.out.println("✉️  Correo: " + estudiante.correo());
         // NO MODIFICAR HASTA AQUÍ
-    }
 
+        InsertBST bst = new InsertBST();
+        int[] values = {5, 3, 7, 2, 4, 6, 8};
+        Node root = null;
+        for (int val : values) {
+            root = bst.insert(root, val);
+        }
+
+        System.out.println("Recorrido preorden del árbol BST:");
+        bst.preOrder(root);
+    }
 }
+
